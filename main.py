28ebb08136ee26d0f6525315cc1c763412fc3874
tapsaca <98756983+tapsaca@users.noreply.github.com>
2025-12-17 23:45:03 +0200

    Add a function for letter count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_letters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,5 +9,8 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
     print(f"Found {num_words} total words")
+    letter_count = count_letters(text)
+    for key, value in letter_count.items():
+        print(f"'{key}': {value}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def count_words(text):
     return len(text.split())
+
+def count_letters(text):
+    letter_count = {}
+    for character in text:
+        if character.isalpha():
+            lower_case_letter = character.lower()
+            if lower_case_letter not in letter_count:
+                letter_count[lower_case_letter] = 0
+            letter_count[lower_case_letter] += 1
+    return letter_count
